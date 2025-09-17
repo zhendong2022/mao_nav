b6a9c58ca654f7c6f4118e8a5112bae6da95d767
zhendong2022 <103576334+zhendong2022@users.noreply.github.com>
2025-09-17 19:32:39 +0400

    chore: 更新导航数据 - 2025/9/17 19:32:39

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -481,7 +481,7 @@ export const mockData = {
       ]
     }
   ],
-  "title": "猫猫导航",
+  "title": "maomao",
   "search": "bing",
-  "_fileSha": "cf9e9f1ad8644add6c0918786cae6707de255aaf"
+  "_fileSha": "3e32499fd88fe787537b9ce08b18612375673d4f"
 }
